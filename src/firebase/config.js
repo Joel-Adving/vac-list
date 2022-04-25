5277b62f5fcb10ac1d7a31e61b522b3c6735f657
Joel-Adving <joel.adving@gmail.com>
2022-04-25 13:37:45 +0200

    progress

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
+import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -9,9 +10,11 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 }
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
 export const db = getFirestore()
 export const auth = getAuth()
+export const analytics = getAnalytics(app)
