8dc9828c6db557ab95c0141922a22481b7dd4a72
Joel Adving <joel.adving@gmail.com>
2022-08-29 22:03:47 +0200

    word break for long names

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,35 +3,35 @@ import { timeAgo } from '../util/timeAgo'
 import Link from 'next/link'
 
 export default function Card({ profile }) {
-    return (
-        <Link href={`/steam-profile/${profile.steamid}`}>
-            <div className="flex flex-col p-4 shadow-2xl cursor-pointer bg-background sm:flex-row">
-                <img className="" src={profile.avatarfull} alt="avatar" />
-                <div className="flex flex-col justify-between flex-1 p-2 space-y-5 sm:p-3 sm:space-y-0">
-                    <h2 className="mt-1 text-4xl sm:text-2xl sm:mt-0 text-highlight">{profile.personaname}</h2>
-                    <div className="text-xl sm:text-lg">
-                        {profile.VACBanned && <p className="text-red-500 ">VAC BANNED</p>}
-                        {profile.NumberOfGameBans > 0 && <p className="text-red-500">GAME BANNED</p>}
-                        {profile.DaysSinceLastBan > 0 && (
-                            <p className="text-red-500">{profile.DaysSinceLastBan} days ago</p>
-                        )}
-                    </div>
-                    <div className="flex space-x-1">
-                        <span className="mr-auto text-sm text-zinc-500">
-                            added {timeAgo.format(new Date(profile.created.seconds * 1000))}
-                        </span>
+  return (
+    <Link href={`/steam-profile/${profile.steamid}`}>
+      <div className="flex flex-col p-4 shadow-2xl cursor-pointer bg-background sm:flex-row">
+        <img className="" src={profile.avatarfull} alt="avatar" />
+        <div className="flex flex-col justify-between flex-1 p-2 space-y-5 sm:p-3 sm:space-y-0">
+          <h2 style={{ wordBreak: 'break-all' }} className="max-w-md mt-1 text-4xl sm:text-2xl sm:mt-0 text-highlight">
+            {profile.personaname}
+          </h2>
+          <div className="text-xl sm:text-lg">
+            {profile.VACBanned && <p className="text-red-500 ">VAC BANNED</p>}
+            {profile.NumberOfGameBans > 0 && <p className="text-red-500">GAME BANNED</p>}
+            {profile.DaysSinceLastBan > 0 && <p className="text-red-500">{profile.DaysSinceLastBan} days ago</p>}
+          </div>
+          <div className="flex space-x-1">
+            <span className="mr-auto text-sm text-zinc-500">
+              added {timeAgo.format(new Date(profile.created.seconds * 1000))}
+            </span>
 
-                        <div className="flex items-center bg-gray-700">
-                            <img
-                                className="w-5 h-5 border-r-[1.5px] border-highlight "
-                                src={profile.added_by.photoURL}
-                                alt="User profile image"
-                            />
-                            <h3 className="mx-1 text-xs text-highlight">{profile.added_by.name.split(' ')[0]}</h3>
-                        </div>
-                    </div>
-                </div>
+            <div className="flex items-center bg-gray-700">
+              <img
+                className="w-5 h-5 border-r-[1.5px] border-highlight "
+                src={profile.added_by.photoURL}
+                alt="User profile image"
+              />
+              <h3 className="mx-1 text-xs text-highlight">{profile.added_by.name.split(' ')[0]}</h3>
             </div>
-        </Link>
-    )
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,28 @@ import { useFilter } from '../hooks/useFilter'
 import { filterByState } from '../atoms/filterByAtom'
 
 export default function Home() {
-    const { loading } = useSteamProfiles()
-    const [filter] = useRecoilState(filterByState)
-    const { filteredProfiles } = useFilter()
+  const { loading } = useSteamProfiles()
+  const [filter] = useRecoilState(filterByState)
+  const { filteredProfiles } = useFilter()
 
-    return (
-        <>
-            <TopNav />
-            <main className="max-w-5xl px-4 mx-auto my-1 md:my-10 text-text-lighter lg:px-0">
-                {filteredProfiles && (
-                    <section className="flex flex-col pt-6 sm:grid gap-14 md:pt-0 sm:gap-3 responsive-grid">
-                        {filter !== 'all' &&
-                            filteredProfiles
-                                .slice()
-                                .sort((a, b) => a?.DaysSinceLastBan - b?.DaysSinceLastBan)
-                                .map(profile => <Card key={profile.steamid} profile={profile} />)}
+  return (
+    <>
+      <TopNav />
+      <main className="max-w-5xl px-4 mx-auto my-1 md:my-10 text-text-lighter lg:px-0">
+        {filteredProfiles && (
+          <section className="flex flex-col pt-6 sm:grid gap-14 md:pt-0 sm:gap-3 responsive-grid">
+            {filter !== 'all' &&
+              filteredProfiles
+                .slice()
+                .sort((a, b) => a?.DaysSinceLastBan - b?.DaysSinceLastBan)
+                .map(profile => <Card key={profile.steamid} profile={profile} />)}
 
-                        {filter === 'all' &&
-                            filteredProfiles.map(profile => <Card key={profile.steamid} profile={profile} />)}
-                    </section>
-                )}
+            {filter === 'all' && filteredProfiles.map(profile => <Card key={profile.steamid} profile={profile} />)}
+          </section>
+        )}
 
-                {loading && <p className="py-12 pb-24 text-4xl font-thin text-cyan-400">Loading...</p>}
-            </main>
-        </>
-    )
+        {loading && <p className="py-12 pb-24 text-4xl font-thin text-cyan-400">Loading...</p>}
+      </main>
+    </>
+  )
 }
